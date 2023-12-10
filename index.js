09384fcf727f4d6d6b3cce6b0cef7225622c334c
miminiverse <minhtamphamtol@gmail.com>
2023-12-09 20:14:59 -0500

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("./stuffThatYouCanIgnore"); // take my word for it :)
 
 app.get("/", (req, res) => {
   res.send(
-    `<h1>Hello world!</h1>
+    `<h1>Hello world</h1>
     <p>
       Thanks for visiting! Your browser is
       <pre>${req.headers["user-agent"]}</pre>
